bdf9ef19e9f465603ca33c6eb4054efa75b22d76
Euan <euanconlon46@gmail.com>
2024-01-19 21:39:22 +0000

    Skills Added

--- skills.js
+++ skills.js
@@ -1,3 +1,27 @@
-const skills = [{
-
-}];
+const skills = [
+    {
+        num: 1,
+        name: "Responsive Web Design",
+        description: "I have lots of projects in web development with many being just practice webpages, however I also have created entire websites as part of my education's coursework"
+    },
+    {
+        num: 2,
+        name: "Database Management",
+        description: "Having worked with databases for a long time, I am confident in querying and manipulating databases whilst working independently or on a server"
+    },
+    {
+        num: 3,
+        name: "Programming Languages",
+        description: "With years of experience in software development, I have used and grew confident with many different languages, also using many styles of programming"
+    },
+    {
+        num: 4,
+        name: "Java OOP & GUI's",
+        description: "Since starting University, I have used Java as a main language for several classes. This has helped me to learn how to use OOP and create GUI's"
+    },
+    {
+        num: 5,
+        name: "Data Structures & Algorithms",
+        description: "I have experimented with many different data structures as well as algorithms in a few languages to develop a firm understanding of them"
+    }
+];
