912db12660e30cb4b3691db9a5c5b77c9b1e0f97
Euan <euanconlon46@gmail.com>
2024-01-25 01:39:35 +0000

    navheader.html Location Change

--- JS/xhr.js
+++ JS/xhr.js
@@ -2,7 +2,7 @@
 window.addEventListener('DOMContentLoaded', function() {
     var navbarPlaceholder = document.getElementById("navheaderPlaceholder");
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "../HTML/navheader.html", true);
+    xhr.open("GET", "navheader.html", true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             navbarPlaceholder.innerHTML = xhr.responseText;
